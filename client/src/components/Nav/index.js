3c58b1f65035c4d8ea2df12d8e420105cc1816f0
Lexi <ambellinger24@gmail.com>
2019-03-16 13:17:00 -0500

    added the changes to the components

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,15 @@
 import React from "react";
+import "./style.css";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="/">
-        React Reading List
+        Google Books
       </a>
+      <a className="nav-link" href="https://www.google.com/">Search<span className="sr-only">(current)</span></a>
+      <a className="nav-link" href="https://www.google.com/">Saved</a>
+
     </nav>
   );
 }
